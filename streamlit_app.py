b7c2edd9c9ae5cdf6ab7dc1b4571e0c6c86f1f0f
devcfcgodoylab <141038527+devcfcgodoylab@users.noreply.github.com>
2023-07-31 10:42:42 -0300

    Update streamlit_app.py
    Updating DF index from my_fruit_list to "Fruits"

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 
 # --- DATA LOADS SESSION ---
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # --- PAGE BUILD SESSION ---
 streamlit.title('My Parents New Healthy Diner')
