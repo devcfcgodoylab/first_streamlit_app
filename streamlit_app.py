1015a2138e0508a80db33992f2c326409d7bf0e6
devcfcgodoylab <141038527+devcfcgodoylab@users.noreply.github.com>
2023-08-01 14:02:00 -0300

    Update streamlit_app.py
    Normalizing response json and printing it

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,12 @@ streamlit.dataframe(fruits_to_show)
 # Advice Session
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# Normalize json from response using pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# Print normalized dataframe from pandas
+streamlit.dataframe(fruityvice_normalized)
+
 
