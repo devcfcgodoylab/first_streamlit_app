ca83a2930ba6745e6f0de0d7a9d14c42c87fc9b5
devcfcgodoylab <141038527+devcfcgodoylab@users.noreply.github.com>
2023-08-02 09:45:27 -0300

    Update streamlit_app.py
    catching errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,32 +29,31 @@ streamlit.dataframe(fruits_to_show)
 # Advice Session
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get more information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLError as e:
+  streamlit.error()
 
-# Normalize json from response using pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-# Print normalized dataframe from pandas
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.stop()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * FROM fruit_load_list")
+#my_data_rows = my_cur.fetchall()
 
-streamlit.stop()
+#streamlit.header("The fruit load list contains!:")
+#streamlit.dataframe(my_data_rows)
 
-# snowflake handshake
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
+#streamlit.write('The user entered the following fruit to be added: ', add_my_fruit)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-
-streamlit.header("The fruit load list contains!:")
-streamlit.dataframe(my_data_rows)
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
-streamlit.write('The user entered the following fruit to be added: ', add_my_fruit)
-
-my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
+#my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
 
 
